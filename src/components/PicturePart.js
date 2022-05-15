3c3071042631d79504ef9ec56bdf4b159071ba7c
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-15 09:56:48 +0700

    static part

---
+++ src/components/PicturePart.js
@@ -0,0 +1,7 @@
+export default function PicturePart(props) {
+    return (
+        <div className="picture-part">
+            <img src="spaceShip.jpg" alt="space ship" />
+        </div>
+    );
+}
---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+
+
+export default function Header() {
+    return (
+        <header className="myHeader">
+            <h1>
+
+                <img src="/logo192.png" alt="logo" width="20$" />
+            </h1>
+            <nav>
+                <ul>
+                    <li>
+                        home
+                    </li>
+                    <li>
+                        about
+                    </li>
+                    <li>
+                        contact
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
