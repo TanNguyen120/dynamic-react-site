e2c0824c9ea1e3e07bfa6951a47d7c4777999611
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-17 10:55:59 +0700

    contact card component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
 import PicturePart from './components/PicturePart';
+import ContactCard from './components/contactCard';
 function App() {
   return (
     <div className="App">
       <Header />
       <PicturePart />
+      <ContactCard />
     </div>
   );
 }
---
+++ src/components/contactCard.js
@@ -0,0 +1,12 @@
+export default function ContactCard() {
+    return (
+        <div className="contactCard">
+            <img src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar" />
+            <div className="contactInfo">
+                <h2>John Doe</h2>
+                <p> +1 (123) 456-7890</p>
+                <p> 123 Le Loi Str, Hanoi, Vietnam</p>
+            </div>
+        </div>
+    );
+}
