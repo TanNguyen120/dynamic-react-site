39806c6ce590227b938b08f42335d63e0ff188eb
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-17 15:59:14 +0700

    fix  style bug

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ function App() {
     <div className="App">
       <Header />
       <PicturePart />
-      <ContactCard name="Bolmer" type="minion" text="affter you play a battlecry repeat the last battlecry that play" />
+      <div className="contact-card">
+        <ContactCard name="Bolmer" type="minion" text="affter you play a battlecry repeat the last battlecry that play" />
+        <ContactCard name="brann" type="minion" text="all battlecry play twice" />
+        <ContactCard name="volcano burst" type="spell" text="deal 15 damages split among all minion" />
+        <ContactCard name="chill wind" type="frost spell" text="freeze a minion and draw a card " />
+      </div>
     </div>
   );
 }
